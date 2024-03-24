d37e60b3681a9d11e0b4a3d661990a3049727dd6
Roberto Núñez <robertonc1599@gmail.com>
2024-03-24 15:50:51 -0500

    Cálculos de Subtotales y Total

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Product } from "@prisma/client";
 import { formatCurrency } from "@/src/utils";
+import AddProductButton from "./AddProductButton";
 
 interface ProductCardProps {
   product: Product;
@@ -20,12 +21,7 @@ export default function ProductCard({ product }: ProductCardProps) {
         <p className="mt-5 font-black text-4xl text-amber-500">
           {formatCurrency(product.price)}
         </p>
-        <button
-          type="button"
-          className="bg-indigo-600 hover:bg-indigo-800 transition-colors duration-500 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
-        >
-          Agregar
-        </button>
+        <AddProductButton product={product} />
       </div>
     </div>
   );
